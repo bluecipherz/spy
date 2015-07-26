5f705ba8d2520097476e8d9b4507ed5e8f8f3b92
Basith Mohammed <basith374@gmail.com>
2015-07-26 19:17:01 +0530

    blh blah bluh

--- spider-selenium.py
+++ spider-selenium.py
@@ -9,31 +9,37 @@ chrome = webdriver.Chrome()
 chrome.get('http://www.bigbasket.com/product/all-categories')
 btn = chrome.find_element_by_id('uiv2-ftv-button')
 btn.click()
-print 'finding shit'
+print 'waiting'
 try:
-    supercats = WebDriverWait(chrome, 10).until(
+    WebDriverWait(chrome, 10).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div[class=dp_headding] a"), '')
-        #chrome.find_elements_by_css_selector("div[class=dp_headding] a")
     )
 finally:
-    print 'shit found'
-for cats in supercats:
-    print cats.get_attribute('href')
-    cats.click()
-    container = chrome.find_element_by_id("products-container")
+    print 'wait finished'
+    supercats = chrome.find_elements_by_css_selector("div[class=dp_headding] a")
+    for cats in supercats:
+        print cats.get_attribute('href')
 
-    products = container.find_elements_by_css_selector("li > .uiv2-list-box-img-title .uiv2-tool-tip-hover")
-    for product in products:
-        print product.text
-    #load = chrome.find_element_by_id('more-products-load')
+        #cats.click()
+        #container = chrome.find_element_by_id("products-container")
+
+        #products = container.find_elements_by_css_selector("li > .uiv2-list-box-img-title .uiv2-tool-tip-hover")
+        #for product in products:
+            #print product.text
+        #load = chrome.find_element_by_id('more-products-load')
+for cats in supercats:
+    cats.click()
+    try:        
+        WebDriverWait(chrome, 10).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div[id=more-products-load]"), '')
+        )
+    finally:
+    #chrome.get(cats.get_attribute('href'))
+        container = chrome.find_element_by_id("products-container")
+
+        products = container.find_elements_by_css_selector("li > .uiv2-list-box-img-title .uiv2-tool-tip-hover")
+        for product in products:
+            print product.text
 
 print 'bye'
-chrome.close()
-#driver.get("http://www.bigbasket.com")
-#assert "Python" in driver.title
-#elem = driver.find_element_by_name("id_q")
-#elem.send_keys("awesome")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-#driver.close()
-
+#chrome.close()
