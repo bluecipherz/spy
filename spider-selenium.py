2e1620d9efc35e475df420cf866b03380f93e44e
Basith Mohammed <basith374@gmail.com>
2015-07-28 22:19:26 +0530

    small changes

--- spider-selenium.py
+++ spider-selenium.py
@@ -3,22 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import any_text_present_in_element
 
 chrome = webdriver.Chrome()
 chrome.get('http://www.bigbasket.com/product/all-categories')
 btn = chrome.find_element_by_id('uiv2-ftv-button')
 btn.click()
-print 'waiting'
 try:
+    print 'waiting'
     WebDriverWait(chrome, 10).until(
-        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div[class=dp_headding] a"), '')
+        any_text_present_in_element((By.CSS_SELECTOR, "div[class=dp_headding] a"))
     )
-finally:
     print 'wait finished'
     supercats = chrome.find_elements_by_css_selector("div[class=dp_headding] a")
     for cats in supercats:
         print cats.get_attribute('href')
-
+except Exception:
+    print 'shite'
         #cats.click()
         #container = chrome.find_element_by_id("products-container")
 
